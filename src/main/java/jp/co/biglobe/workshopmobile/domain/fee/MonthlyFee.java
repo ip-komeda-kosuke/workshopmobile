6dde269c1132b5d0a7ce6e140a68ad7721f9cdc6
komeda_kosuke <komeda_kosuke@interprism.co.jp>
2022-03-11 11:28:50 +0900

    refactor

--- src/main/java/jp/co/biglobe/workshopmobile/domain/fee/MonthlyFee.java
+++ src/main/java/jp/co/biglobe/workshopmobile/domain/fee/MonthlyFee.java
@@ -11,7 +11,7 @@ public class MonthlyFee {
     @Getter
     private final int value;
 
-    public MonthlyFee plus(MonthlyFee monthlyFee){
+    public MonthlyFee plus(MonthlyFee monthlyFee) {
         return new MonthlyFee(value + monthlyFee.getValue());
     }
 }
